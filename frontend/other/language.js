6a0cea57c3d412bcccdbeeadacee55732a0e819b
felix.scriptworld <felix@scriptworld.net>
2018-11-17 23:27:55 +0100

    update language for post

--- frontend/other/language.js
+++ frontend/other/language.js
@@ -1,6 +1,31 @@
 class Language {
 	constructor() {
-		this.dict = {};
+		this.dict = {
+			NAVBAR_HOT: 'hot',
+			NAVBAR_RECENT: 'recent',
+			NAVBAR_LOGIN: 'login',
+			NAVBAR_LOGOUT: 'logout',
+
+			POST_POSTED_BY: 'posted by',
+			POST_AGO: 'ago',
+
+			SECOND: 'second',
+			SECONDS: 'seconds',
+			MINUTE: 'minute',
+			MINUTES: 'minutes',
+			HOUR: 'hour',
+			HOURS: 'hours',
+			DAY: 'day',
+			DAYS: 'days',
+			MONTH: 'month',
+			MONTHS: 'months',
+			YEAR: 'year',
+			YEARS: 'years',
+
+			NO: 'no',
+			COMMENT: 'comment',
+			COMMENTS: 'comments',
+		};
 	}
 
 	get(ident) {
